e68527f6cfe506d9dadfed961323033327ef917f
J T <git@tombre.us>
2015-08-04 23:38:00 -0400

    Initial outline to track running kali services in a terminal window

---
+++ scripts/kali-status.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# File: kali-status.bash
+#
+# Description:
+#   Provide a visual of services that are running 
+#   service postgresql start
+#   service metasploit start
+#   service ssh start
