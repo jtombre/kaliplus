f327ececb9846fdbe966bdf979c55736ea4205d7
J T <git@tombre.us>
2015-08-05 22:05:48 -0400

    Return apt-get status after each command

--- scripts/kali-update.bash
+++ scripts/kali-update.bash
@@ -12,4 +12,36 @@ if [[ $EUID -ne 0 ]]; then
 fi
 # Update the kali distro
 echo "Updating kali..."
-apt-get clean && apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y
+#apt-get clean && apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y
+
+# Add some additional functionality
+echo "apt-get clean..."
+if apt-get clean -q; then
+   echo "[+] apt-get clean successful"
+else
+   echo "[-] apt-get clean had failuers"
+fi
+
+# Add some additional functionality
+echo "apt-get update..."
+if apt-get update -qq; then
+   echo "[+] apt-get update successful"
+else
+   echo "[-] apt-get update had failuers"
+fi
+
+# Add some additional functionality
+echo "apt-get upgrade..."
+if apt-get upgrade -y -qq ; then
+   echo "[+] apt-get upgrade successful"
+else
+   echo "[-] apt-get upgrade had failuers"
+fi
+
+# Add some additional functionality
+echo "apt-get -q dist-upgrade..."
+if apt-get dist-upgrade -qq -y; then
+   echo "[+] apt-get dist-upgrade successful"
+else
+   echo "[-] apt-get dist-upgrade had failuers"
+fi
